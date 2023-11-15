ea0e90d8a210c27f2dfe6da619debe624b7d5576
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-15 15:18:24 +0300

    implement search

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,26 +1,49 @@
-import React from "react";
-import products from "../data/items.json";
+import React, { useEffect, useState } from "react";
+import items from "../data/items.json";
 import categories from "../data/categories.json";
+import { Link } from "react-router-dom";
 import { Trash2, Pencil } from "lucide-react";
 const Dashboard = () => {
+  const [products, setProducts] = useState(items);
+  const [search, setSearch] = useState("");
 
+  // filter category using category id on products
+  const extractCategoryName = (id) => {
+    const category = categories.find((category) => category.id === id);
+    return category.name;
+  };
 
-    // filter category using category id on products
-    const extractCategoryName = (id) => {
-        const category = categories.find((category) => category.id === id);
-        return category.name;
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    if (search === "") {
+      setProducts(items);
+    } else {
+      const filteredProducts = products.filter((product) =>
+        product.name.toLowerCase().includes(search.toLowerCase())
+      );
+      setProducts(filteredProducts);
     }
+  };
+  const handleReset = () => {
+      setSearch("");
+    setProducts(items);
+  };
 
   return (
     <div className="bg-white w-full h-auto rounded px-20 mt-3 ">
-      <div className="flex items-center justify-center gap-10 pt-5">
+          <div className="flex items-center justify-center gap-10 pt-5">
+          
         <input
-          type="search"
+          onChange={handleSearch}
+          type="text"
           placeholder="search"
           className="text-blue-400 border px-4 py-1 rounded outline-none"
         />
-        <button className="bg-blue-400 text-white px-3 py-1 rounded">
-          Search
+        <button
+          onClick={handleReset}
+          className="bg-blue-400 text-white px-3 py-1 rounded"
+        >
+          Clear
         </button>
       </div>
       <div className="mt-3 w-full p-5">
@@ -44,9 +67,9 @@ const Dashboard = () => {
                 <td>{product.quantity}</td>
                 <td>{product.price}</td>
                 <td className="flex gap-4 items-center justify-center">
-                  <button>
+                        <Link to={`edit-item/${product.id}`}>
                     <Pencil size={20} color="#9370f5" />
-                  </button>
+                  </Link>
                   <button>
                     <Trash2 size={20} color="#b5306a" />
                   </button>
